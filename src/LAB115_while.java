006dd057eea0ea9d332ed5ffc5ff9df151512447
atlasrikrisha <atlasrikrisha@gmail.com>
2025-10-02 21:17:11 +0530

    while 1

---
+++ src/LAB115_while.java
@@ -0,0 +1,9 @@
+public class LAB115_while {
+    public static void main(String[] args) {
+        int i = 0;//init
+        while (i < 10) { //condition (must be true to enter the loop)
+            System.out.println(i);
+            i++;//updation
+        }
+    }
+}
