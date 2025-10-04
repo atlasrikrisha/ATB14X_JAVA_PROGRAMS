3b61821d7a88ed37764cf53a2ea964e331226469
atlasrikrisha <atlasrikrisha@gmail.com>
2025-10-04 16:08:27 +0530

    fu

---
+++ src/ex_13_functions/lab128_functions.java
@@ -0,0 +1,13 @@
+package ex_13_functions;
+
+
+
+
+public class lab128_functions {
+    public static void main(String[] args) {
+        int max = Math.max(32,24);
+        //built - in
+
+        System.out.println(max);
+    }
+}
